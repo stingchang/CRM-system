5abf7fd2716c246ffa4ad4d4082451fbd58d57c8
sting <sting.changh@gmail.com>
2017-08-17 22:07:12 -0700

    fix typo

--- src/com/spring/demo/controller/CustomerControler.java
+++ src/com/spring/demo/controller/CustomerControler.java
@@ -1,6 +1,5 @@
 package com.spring.demo.controller;
 
-import com.spring.demo.dao.CustomerDAO;
 import com.spring.demo.entity.Customer;
 import com.spring.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
